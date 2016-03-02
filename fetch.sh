ceab34bbcb8bbbe5f973fbf78dd96a7c8dc86bdc
Niklas Keller <me@kelunik.com>
2016-03-02 12:42:24 +0100

    Remove escaped slashes

--- fetch.sh
+++ fetch.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-mkdir -p _data
-curl -s "https://packagist.org/search.json?tags=amphp" > _data/tagged.json
+# Jekyll 3.0 doesn't support escaped slashes…
+curl -s "https://packagist.org/search.json?tags=amphp" | sed 's/\\\//\//g' > _data/tagged.json
